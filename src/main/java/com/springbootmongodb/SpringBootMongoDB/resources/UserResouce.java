4f8bc8abad89d3660a77a084265c5aa2a25776c0
Adriano Crosoletto <adrianocrosoletto@outlook.com>
2022-07-11 17:44:28 -0300

    Entity User e Rest OK

---
+++ src/main/java/com/springbootmongodb/SpringBootMongoDB/resources/UserResouce.java
@@ -0,0 +1,27 @@
+package com.springbootmongodb.SpringBootMongoDB.resources;
+
+import com.springbootmongodb.SpringBootMongoDB.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResouce {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<User>> findAll(){
+        User maria = new User("1","Maria Silva","maria@gmail.com");
+        User alex = new User("2","Alex Santos","alex@gmail.com");
+        List<User> list = new ArrayList<>();
+        list.addAll(Arrays.asList(maria,alex));
+
+        return ResponseEntity.ok().body(list);
+    }
+
+}
