9af8473d64ba0d30ce7f6a0384a91c681d1b31a7
Adriano Crosoletto <adrianocrosoletto@outlook.com>
2022-07-12 00:43:03 -0300

    consulta com varios criterios

--- src/main/java/com/springbootmongodb/SpringBootMongoDB/services/PostService.java
+++ src/main/java/com/springbootmongodb/SpringBootMongoDB/services/PostService.java
@@ -9,6 +9,7 @@ import com.springbootmongodb.SpringBootMongoDB.services.exception.ObjectNotFound
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,15 +17,20 @@ import java.util.Optional;
 public class PostService {
 
     @Autowired //Ingeção de dependencia
-    private PostRepository postRepository;
+    private PostRepository repo;
 
       public Post findById(String id){
-        Optional<Post> obj = postRepository.findById(id);
+        Optional<Post> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado !"));
     }
 
     public List<Post> findByTitle(String text){
-          return postRepository.searchTitle(text);
+          return repo.searchTitle(text);
     }
+
+    public List<Post>fullSearch(String text, Date minDate,Date maxDate){
+          maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+            return repo.fullSearch(text,minDate,maxDate);
+      }
 
 }
